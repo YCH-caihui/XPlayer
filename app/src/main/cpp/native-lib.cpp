c7dda27ed3610f5ccf84a52fea2cd33bbf835469
YCH-caihui <chyangbest@icloud.com>
2020-11-23 13:38:09 +0800

    添加解封装过程

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,11 @@ extern "C" {
 
 }
 
+
+double r2d(AVRational rational) {
+    return rational.den == 0 ? 0 : (double )rational.num / (double )rational.den ;
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_caihui_xplayer_MainActivity_stringFromJNI(
         JNIEnv* env,
@@ -28,21 +33,58 @@ Java_com_caihui_xplayer_MainActivity_stringFromJNI(
     int totalMs = ic -> duration / AV_TIME_BASE / 1000;
     __android_log_print(ANDROID_LOG_INFO, "ffcaihui",  "duration: %d", totalMs);
 
-
-    av_dump_format(ic, 0, nullptr, 0);
-
+    int videoStreamIndex = -1;
+    int audioStreamIndex = -1;
 
     //获取音视频流信息
     for( int i = 0; i < ic->nb_streams; i++ ) {
         AVStream *as = ic->streams[i];
-        if(as->codecpar->codec_type = AVMEDIA_TYPE_AUDIO) {
+        __android_log_print(ANDROID_LOG_DEBUG, "ffcaihui", " streams index:%d   codecType: %d", i,AVMEDIA_TYPE_VIDEO);
+        if(as->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audioStreamIndex = i;
             //音频信息
+            __android_log_print(ANDROID_LOG_INFO, "ffcaihui", "sample_rate: %d", as->codecpar->sample_rate);
+
         } else if(as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStreamIndex = i;
             //视频信息
+            __android_log_print(ANDROID_LOG_INFO, "ffcaihui", "width: %d  height: %d", as->codecpar->width, as->codecpar->height);
+            //帧率
+            __android_log_print(ANDROID_LOG_DEBUG, "ffcaihui", "frame %f  frameSize %d", r2d(as->avg_frame_rate), as->codecpar->frame_size);
+
         }
     }
 
+    //malloc AVPacket 并初始化
+    AVPacket  *pkt = av_packet_alloc();
 
+    while(true) {
+        int re = av_read_frame(ic, pkt);
+        if(re != 0) {
+            int ms = 3000;
+            long long pos = (double)ms / (double)1000 * r2d(ic->streams[pkt->stream_index]->time_base);
+            av_seek_frame(ic, pkt->stream_index, pos, AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_FRAME );
+            break;
+        }
+
+
+        if(pkt->stream_index == videoStreamIndex) {
+            //视频
+            __android_log_print(ANDROID_LOG_INFO, "ffcaihui", "视频");
+        } else if(pkt->stream_index == audioStreamIndex) {
+            //音频
+            __android_log_print(ANDROID_LOG_INFO, "ffcaihui", "音频");
+        }
+
+        __android_log_print(ANDROID_LOG_INFO, "ffcaihui", "pktsize: %d  pts:%lld   dts:%lld   realTime:%f ", pkt->size, pkt->pts, pkt->dts,
+                            pkt->pts * r2d(ic->streams[pkt->stream_index]->time_base)) * 1000;
+
+
+        //释放引用计数-1. 为0释放空间
+        av_packet_unref(pkt);
+    }
+
+    av_packet_free(&pkt);
 
     if(ic){
         avformat_close_input(&ic);
